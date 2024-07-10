af0bc08261c688ae49990a833ff97837e6110e3b
Neha Sawant <109304726+nehasawant4@users.noreply.github.com>
2024-07-09 17:25:55 -0700

    app

--- app.py
+++ app.py
@@ -1,54 +1,50 @@
-import sys
-print(sys.executable)
-
 import streamlit as st
 from langchain.prompts import PromptTemplate
 from langchain_community.llms import CTransformers
 
-## Function to get response from LLAMA 2 model
+# Function to get response from LLaMa 2 model
 
-def getLlamaResponse(input_text,no_words,blog_style):
-    ## Llama2 model
+def getLLamaResponse(input_text, blog_style):
 
-    llm=CTransformers(model='models/llama-2-7b-chat.ggmlv3.q8_0.bin',
-                      model_type='llama',
-                      config={'max_new_tokens': 256,
-                              'temperature':0.01})
+    # LLama2 model
+    llm = CTransformers(model='models/llama-2-7b-chat.ggmlv3.q8_0.bin',
+                        model_type = 'llama',
+                        config = {'max_new_tokens': 256,
+                                  'temperature': 0.01})
 
-    ## Prompt Template
-    template=f"""
-    Write a blog for {blog_style} for a topic {input_text} within {no_words}.
+    # Enhanced Prompt Template
+    template = """
+    Create a comprehensive blog post tailored for needs of a {blog_style} job profile on the topic "{input_text}". Write the blog with engaging title that captures the reader's interest and blog content under 1000 words.
+    Also give references, links to credible sources, articles, or studies that support the content.
     """
 
-    prompt=PromptTemplate(input_variables=["blog_style","input_text","no_words"],
-                          template=template)
+    prompt = PromptTemplate(input_variables = ['blog_style', 'input_text'],
+                            template=template)
 
-    ## Generate the responses from Llama2 model
-    response=llm(prompt.format(blog_style=blog_style, input_text=input_text, no_words=no_words))
+    # Generate a response from LLama2 model
+    response = llm.invoke(prompt.format(blog_style=blog_style, input_text=input_text))
+
     print(response)
     return response
 
-st.set_page_config(page_title='Generate Blogs',
-                   page_icon='🤖',
-                   layout='centered',
-                   initial_sidebar_state='collapsed')
+
+st.set_page_config(page_title = 'Generate Blogs',
+                   page_icon = '🤖',
+                   layout = 'centered',
+                   initial_sidebar_state = 'collapsed')
 
 st.header("Generate Blogs 🤖")
 
-input_text=st.text_input('Enter the Blog Topic')
+input_text = st.text_input("Enter the blog topic")
 
-## Creating columns
+# Creating two more columns for addtional 2 fields
 
-col1, col2=st.columns([5,5])
+blog_style = st.selectbox('Writing the blog for',
+                              ('Researcher', 'Lecturer', 'Student'), index=0)
 
-with col1:
-    no_words=st.text_input('No of Words')
+submit = st.button('Generate')
 
-with col2:
-    blog_style=st.selectbox('Writing the Blog for', ('Researchers', 'Data Scientist','Students'),index=0)
+# Final Response:
 
-submit=st.button('Generate')
-
-## Final response
 if submit:
-    st.write(getLlamaResponse(input_text, no_words, blog_style))
+    st.write(getLLamaResponse(input_text, blog_style))
